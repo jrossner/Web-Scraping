94707fdab09807ba3cc448dbe589f1d3f8528d12
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-09-06 13:46:41 -0400

    Update espnHeadlinesNotifier.py

--- projects/emailing/espn-headlines/espnHeadlinesNotifier.py
+++ projects/emailing/espn-headlines/espnHeadlinesNotifier.py
@@ -3,10 +3,13 @@ import smtplib
 from scrapeHeadlines import check_headlines
 from emailSender import send_email
 
+username = os.environ.get('ESPN_EMAILER_USER')
+passcode = os.environ.get('ESPN_EMAILER_PASS')
+  
 try:
   hl = check_headlines("nfl",["buffalo","bill"])
   print(f'in eHN: {hl}')
-  send_email(hl)
+  send_email(hl,username,passcode)
   print("Process executed.")
 except Exception as ex:
   print(f'Issue encountered... could not execute due to: {ex}')
