26f94d8d9f0f3345f37583f29c1e3e5f7da7a2cb
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-09-06 13:45:57 -0400

    Update emailSender.py

--- projects/emailing/espn-headlines/emailSender.py
+++ projects/emailing/espn-headlines/emailSender.py
@@ -2,11 +2,8 @@ import os
 import smtplib
 from email.message import EmailMessage
 
-def send_email(headlineBody):
-  username = os.environ.get('ESPN_EMAILER_USER')
-  passcode = os.environ.get('ESPN_EMAILER_PASS')
+def send_email(headlineBody,username,passcode):
   message = EmailMessage()
-  
   message['To'] = 'rosj016@gmail.com'
   message['Subject'] = 'ESPN Headlines For You'
   message['From'] = username
