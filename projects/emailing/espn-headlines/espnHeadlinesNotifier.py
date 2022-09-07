1eef5147e56711df168b6bcc8ae2d1d603f62672
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-09-07 08:50:08 -0400

    Update espnHeadlinesNotifier.py

--- projects/emailing/espn-headlines/espnHeadlinesNotifier.py
+++ projects/emailing/espn-headlines/espnHeadlinesNotifier.py
@@ -3,8 +3,8 @@ import smtplib
 from scrapeHeadlines import check_headlines
 from emailSender import send_email
 
-username = os.environ.get('ESPN_EMAILER_USER')
-passcode = os.environ.get('ESPN_EMAILER_PASS')
+username = os.environ.get('EMAIL_USERNAME')
+passcode = os.environ.get('EMAIL_PASSCODE')
   
 try:
   send_email(check_headlines("nfl",["buffalo","bill"]),username,passcode)
