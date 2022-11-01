46b8911cfe74ebed96934340ae690d1c932a6d1d
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-01 19:43:59 -0400

    Update sendUpcomingExDividendTweet.py

--- projects/tweeting/applereitnews/ex-dividend/sendUpcomingExDividendTweet.py
+++ projects/tweeting/applereitnews/ex-dividend/sendUpcomingExDividendTweet.py
@@ -4,10 +4,10 @@ import tweepy
 from check_upcoming_ex_dividend import checkForUpcomingExDividend
 
 # SECRETS
-API_KEY = os.environ.get('APPLEREITNEWS_API_KEY')
-API_SECRET = os.environ.get('APPLEREITNEWS_API_SECRET')
-ACCESS_TOKEN = os.environ.get('APPLEREITNEWS_ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.environ.get('APPLEREITNEWS_ACCESS_TOKEN_SECRET')
+API_KEY = str(os.environ.get('APPLEREITNEWS_API_KEY'))
+API_SECRET = str(os.environ.get('APPLEREITNEWS_API_SECRET'))
+ACCESS_TOKEN = str(os.environ.get('APPLEREITNEWS_ACCESS_TOKEN'))
+ACCESS_TOKEN_SECRET = str(os.environ.get('APPLEREITNEWS_ACCESS_TOKEN_SECRET'))
 
 # AUTHORIZATION
 auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
