ff173762b02dfadd61532c8981a81651370c9eb2
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-01 13:22:41 -0400

    Update check_upcoming_ex_dividend.py

--- projects/tweeting/applereitnews/ex-dividend/check_upcoming_ex_dividend.py
+++ projects/tweeting/applereitnews/ex-dividend/check_upcoming_ex_dividend.py
@@ -7,7 +7,7 @@ def checkForUpcomingExDividend():
   today = datetime.today()
   
   if lastDayToOwn == (today+timedelta(days=1)).strftime('%Y-%m-%d'):
-    text = "Tomorrow is APLE's Ex-Dividend Date.\nThis is the last day to be recorded as an owner of APLE stock to receive the next dividend."
+    text = "Tomorrow is Apple REIT's Ex-Dividend Date.\nWhoever owns APLE stock at the end of today will receive the next dividend."
   else:
     text = False
 
