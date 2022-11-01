06bcafdef8a2b0a17fdf031ec6de070a82e611b9
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-01 10:41:18 -0400

    Create get_closing_price.py

---
+++ projects/tweeting/applereitnews/get_closing_price.py
@@ -0,0 +1,42 @@
+import yfinance as yf
+from datetime import datetime
+
+def getClosingPrice():
+  info = yf.Ticker("APLE").history(period="365d")
+  today = datetime.today().strftime('%Y-%m-%d')
+  last = info.tail(1).index.item().strftime('%Y-%m-%d')
+  readableDate = datetime.today().strftime('%A %B %-d')
+  
+  if (today == last):
+      closing = info["Close"][-1]
+      changePerc = 100 * ((info["Close"][-1] - info["Close"][-2]) / info["Close"][-2])
+      if changePerc < 0:
+          opp = ''
+      else:
+          opp = '+'
+      text = f'{readableDate}: Closing Price: ${round(closing,2)} ({opp}{round(change,2)}%)'
+
+      monthChangeAmount = info["Close"][-1] - info["Close"][-30]
+      monthChangePerc = monthChangeAmount / info["Close"][-30]
+
+      if monthChangeAmount < 0:
+          monthOpp = '-'
+      else: 
+          monthOpp = '+'
+
+      text += f'\n1 Month Change: {monthOpp}${round(monthChangeAmount,2)} ({monthOpp}{round(monthChangePerc,2)}%)'
+
+      yearChangeAmount = info["Close"][-1] - info["Close"][-365]
+      yearChangePerc = 100 * (yearChangeAmount / info["Close"][-365])
+
+      if yearChangeAmount < 0:
+          yearOpp = '-'
+      else: 
+          yearOpp = '+'
+
+      text += f'\n1 Year Change: {yearOpp}${round(yearChangeAmount,2)} ({yearOpp}{round(yearChangePerc,2)}%)'
+
+  else:
+      text = f'{readableDate}: Market Was Closed Today'
+      
+  return text
