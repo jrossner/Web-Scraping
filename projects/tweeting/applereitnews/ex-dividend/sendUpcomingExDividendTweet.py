8dc0091f88ec0cbb85263baed923ed8a2adb3844
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-01 13:15:35 -0400

    Update sendUpcomingExDividendTweet.py

--- projects/tweeting/applereitnews/ex-dividend/sendUpcomingExDividendTweet.py
+++ projects/tweeting/applereitnews/ex-dividend/sendUpcomingExDividendTweet.py
@@ -1,1 +1,14 @@
-sendUpcomingExDividendTweet
+import yfinance as yf
+from datetime import datetime, timedelta
+
+def checkForUpcomingExDividend():
+  exDividendEpoch = yf.Ticker("APLE").info['exDividendDate']
+  lastDayToOwn = datetime.fromtimestamp(exDividendEpoch).strftime('%Y-%m-%d')
+  today = datetime.today()
+  
+  if lastDayToOwn == (today+timedelta(days=1)).strftime('%Y-%m-%d'):
+    text = "Tomorrow is APLE's Ex-Dividend Date.\nThis is the last day to be recorded as an owner of APLE stock to receive the next dividend."
+  else:
+    text = False
+
+  return text
