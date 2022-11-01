6999751633007d35be2fb931a949895d2dcf4748
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-01 12:45:20 -0400

    Update sendTweet.py

--- projects/tweeting/applereitnews/sendTweet.py
+++ projects/tweeting/applereitnews/sendTweet.py
@@ -4,14 +4,14 @@ import tweepy
 from get_closing_price import getClosingPrice
 
 # SECRETS
-API_KEY = os.environ.get('API_KEY')
-API_SECRET = os.environ.get('API_SECRET')
-ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+API_KEY = os.environ.get('APPLEREITNEWS_API_KEY')
+API_SECRET = os.environ.get('APPLEREITNEWS_API_SECRET')
+ACCESS_TOKEN = os.environ.get('APPLEREITNEWS_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('APPLEREITNEWS_ACCESS_TOKEN_SECRET')
 
 # AUTHORIZATION
-auth = tweepy.OAuthHandler(APPLEREITNEWS_API_KEY,APPLEREITNEWS_API_SECRET)
-auth.set_access_token(APPLEREITNEWS_ACCESS_TOKEN,APPLEREITNEWS_ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 try:
