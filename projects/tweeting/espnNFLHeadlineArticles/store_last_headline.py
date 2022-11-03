63342bfdd8ad9f52c8ab37c3f2c83942a0ee8aa6
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-03 11:35:19 -0400

    Update store_last_headline.py

--- projects/tweeting/espnNFLHeadlineArticles/store_last_headline.py
+++ projects/tweeting/espnNFLHeadlineArticles/store_last_headline.py
@@ -1,12 +1,14 @@
 import json
 
 def storeLastHeadline(newHeadlines):
-  print("check 1")
   last_headline = newHeadlines[list(newHeadlines.keys())[0]]
-  print("check 2")
-  print(f'last_headline: {last_headline}')
   object = json.dumps(last_headline)
   
-  with open('last_headline.json', 'w') as outfile:
-    outfile.write(object)
+  try:
+    with open('last_headline.json', 'w') as outfile:
+      outfile.write(object)
+      print('json file written successfully')
+    except Exception as ex:
+      print(f'Could not write json file due to: {ex}')
+    
   
