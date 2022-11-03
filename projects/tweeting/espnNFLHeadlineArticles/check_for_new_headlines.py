8c2eded3a325c0a365f92ed5cabb96a28e326bd0
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-03 10:45:09 -0400

    Update check_for_new_headlines.py

--- projects/tweeting/espnNFLHeadlineArticles/check_for_new_headlines.py
+++ projects/tweeting/espnNFLHeadlineArticles/check_for_new_headlines.py
@@ -3,7 +3,7 @@ import json
 def checkIfNewHeadlines(articles,lastHeadline):
     newHeadlines = []
     for key in articles.keys():
-        if key == lastHeadline.keys()[0]:
+        if key == list(lastHeadline.keys())[0]:
             break
         else:
             newHeadlines.append(articles[key])
