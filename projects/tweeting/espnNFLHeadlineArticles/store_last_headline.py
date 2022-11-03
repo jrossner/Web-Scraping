c135c30138a7bf5ebf094c89cc0f3a074c3ddb3c
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-03 11:18:44 -0400

    Update store_last_headline.py

--- projects/tweeting/espnNFLHeadlineArticles/store_last_headline.py
+++ projects/tweeting/espnNFLHeadlineArticles/store_last_headline.py
@@ -2,9 +2,9 @@ import json
 
 def storeLastHeadline(newHeadlines):
   print("check 1")
-  last_headline = {newHeadlines[list(newHeadlines.keys())[0]]}
-  
-  print(f'last_headline: {str(last_headline)}')
+  last_headline = newHeadlines[list(newHeadlines.keys())[0]]
+  print("check 2")
+  print(f'last_headline: {last_headline}')
   object = json.dumps(last_headline)
   
   with open('last_headline.json', 'w') as outfile:
