5fd64891978b4ff451194761572531f1f2b1611b
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-11-03 10:15:41 -0400

    Update checkForNewHeadlinesAndTweet.py

--- projects/tweeting/espnNFLHeadlineArticles/checkForNewHeadlinesAndTweet.py
+++ projects/tweeting/espnNFLHeadlineArticles/checkForNewHeadlinesAndTweet.py
@@ -1,6 +1,22 @@
+from get_espn_nfl_headlines import getEspnNFLHeadlines
+from check_for_new_headlines import checkIfNewHeadlines
+from send_new_headline_tweets import sendNewHeadlineTweets
 
+# SECRETS
+API_KEY = f"{os.environ.get('API_KEY')}"
+API_SECRET = f"{os.environ.get('API_SECRET')}"
+ACCESS_TOKEN = f"{os.environ.get('ACCESS_TOKEN')}"
+ACCESS_TOKEN_SECRET = f"{os.environ.get('ACCESS_TOKEN_SECRET')}"
 
 try:
-  lastHeadline = json.load(open('last_headline.json'))
+  last_headline = json.load(open('last_headline.json'))
 except:
-  lastHeadline = {}
+  last_headline = {}
+  
+try:
+  current_headlines = getEspnNFLHeadlines()
+  new_headlines = checkIfNewHeadlines(current_headlines,last_headline)
+  sendNewHeadlineTweets(new_headlines,API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+  print("Script completed")
+except Exception as ex:
+  print(f'Could not complete script due to: {ex}'
