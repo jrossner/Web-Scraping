c68e1ffe9c116bd11526aaf972c724e4b44397c2
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2023-01-31 18:19:34 -0500

    Update get_closing_price.py

--- projects/tweeting/applereitnews/closing-price/get_closing_price.py
+++ projects/tweeting/applereitnews/closing-price/get_closing_price.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 from datetime import datetime
-from ..\dateSuffix import getDateSuffix
+from ..dateSuffix import getDateSuffix
 
 def getClosingPrice():
   info = yf.Ticker("APLE").history(period="365d")
