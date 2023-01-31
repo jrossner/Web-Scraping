be561e0fa85fc9f9ffdd172efa2c895902f25c29
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2023-01-31 18:34:48 -0500

    Update check_upcoming_ex_dividend.py

--- projects/tweeting/applereitnews/ex-dividend/check_upcoming_ex_dividend.py
+++ projects/tweeting/applereitnews/ex-dividend/check_upcoming_ex_dividend.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 from datetime import datetime, timedelta
-from ../dateSuffix import getDateSuffix
+#from ../dateSuffix import getDateSuffix
 
 def checkForUpcomingExDividend():
   exDividendEpoch = yf.Ticker("APLE").info['exDividendDate']
@@ -10,8 +10,9 @@ def checkForUpcomingExDividend():
   if lastDayToOwn == (today+timedelta(days=1)).strftime('%Y-%m-%d'):
     text = "Tomorrow is Apple REIT's Ex-Dividend Date.\n\nWhoever owns APLE stock at the end of today will receive the next dividend."
   elif lastDayToOwn == (today+timedelta(days=7)).strftime('%Y-%m-%d'):
-    dateSuf = getDate((today+timedelta(days=6)).strftime('%d'))
-    text = f"Apple REIT's Ex-Dividend Date is 1 Week from today. \n\nWhoever owns APLE stock by the end of {(today+timedelta(days=6)).strftime('%A %B %d')}{dateSuf} will receive the next dividend."
+    #dateSuf = getDate((today+timedelta(days=6)).strftime('%d'))
+    #text = f"Apple REIT's Ex-Dividend Date is 1 Week from today. \n\nWhoever owns APLE stock by the end of {(today+timedelta(days=6)).strftime('%A %B %d')}{dateSuf} will receive the next dividend."
+    text = f"Apple REIT's Ex-Dividend Date is 1 Week from today. \n\nWhoever owns APLE stock by the end of {(today+timedelta(days=6)).strftime('%A %B %d')} will receive the next dividend."
   else:
     text = False
 
