b9261dad930f677e8952835695a4b797a6209a1d
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2023-02-01 09:41:22 -0500

    Update check_for_upcoming_earnings.py

--- projects/tweeting/applereitnews/upcoming-earnings/check_for_upcoming_earnings.py
+++ projects/tweeting/applereitnews/upcoming-earnings/check_for_upcoming_earnings.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import os
 try:
   os.chdir('..')
-  os.chdir('/utility_functions/')
+  os.chdir('utility_functions')
   from dateSuffix import getDateSuffix
 except Exception as ex:
   print(f'Didnt work: {ex}')
