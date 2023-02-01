ee93bde01ff7056f07e9d6fcd1c520ed1ff5925c
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2023-02-01 08:05:37 -0500

    Update check_for_upcoming_earnings.py

--- projects/tweeting/applereitnews/upcoming-earnings/check_for_upcoming_earnings.py
+++ projects/tweeting/applereitnews/upcoming-earnings/check_for_upcoming_earnings.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime, timedelta
-from ...dateSuffix import getDateSuffix
+from ...applereitnews.dateSuffix import getDateSuffix
 
 def checkUpcomingEarnings():
   link = "https://www.marketbeat.com/stocks/NYSE/APLE/earnings/"
